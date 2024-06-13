d09109d903c74bf1ba324f2173f721f076a7bca7
Kort-Gebert <kort.gebert.2020@gmail.com>
2024-06-13 01:32:14 -0600

    updated files

--- src/finalProject/Card.java
+++ src/finalProject/Card.java
@@ -5,41 +5,42 @@ public class Card {
 	private String cardName;
 	private String suitName;
 	private int value;
-	
+
 //Card Constructor	
 	public Card(String cardName, String suitName, int value) {
 		this.cardName = cardName;
 		this.suitName = suitName;
-		this.value = value;	
+		this.value = value;
 	}
+
 //Method to display a card	
 	public void displayCard() {
 		System.out.println(cardName + " of " + suitName + " -- " + value);
 	}
+
 //Getters and Setters	
 	public void SetCardName(String cardName) {
 		this.cardName = cardName;
 	}
-	
+
 	public String getCardName() {
-	return cardName;	
+		return cardName;
 	}
-	
+
 	public void setSuitName(String suitName) {
 		this.suitName = suitName;
 	}
-	
+
 	public String getSuitName() {
 		return suitName;
 	}
-	
+
 	public void SetValue(int value) {
 		this.value = value;
 	}
-	
+
 	public int getValue() {
 		return value;
 	}
-	
-	
-	}
+
+}
--- src/finalProject/Deck.java
+++ src/finalProject/Deck.java
@@ -5,48 +5,50 @@ import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-	//List of cards for deck
+	// List of cards for deck
 	List<Card> cards = new ArrayList<Card>();
-	//Deck constructor
-public Deck() {
-	 String[] suits = {"Diamonds", "Hearts", "Spades", "Clubs"};
-	 String[] numbers = {"Two", "Three", "Four", "Five", "Six", "Seven",
-			      "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
-	 
-	 for (String suit : suits) {
-		 int count = 2;
-		 for (String number : numbers) {
-			 Card card = new Card(number, suit, count);
-			 this.cards.add(card);
-			 count++;
-		 }
-	 }	 
-}
-//Method to display deck 
-public void displayDeck() {
-	for(Card card : this.cards) {
-		card.displayCard();
+
+	// Deck constructor
+	public Deck() {
+		String[] suits = { "Diamonds", "Hearts", "Spades", "Clubs" };
+		String[] numbers = { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen",
+				"King", "Ace" };
+
+		for (String suit : suits) {
+			int count = 2;
+			for (String number : numbers) {
+				Card card = new Card(number, suit, count);
+				this.cards.add(card);
+				count++;
+			}
+		}
 	}
-}
+
+//Method to display deck 
+	public void displayDeck() {
+		for (Card card : this.cards) {
+			card.displayCard();
+		}
+	}
+
 //Method to draw card
-public Card draw() {
-	Card card = this.cards.remove(0);
-	return card;
-}
+	public Card draw() {
+		Card card = this.cards.remove(0);
+		return card;
+	}
+
 //Method to shuffle the deck
-public void shuffle() {
-	Collections.shuffle(this.cards);
-}
+	public void shuffle() {
+		Collections.shuffle(this.cards);
+	}
+
 //Getters and Setters
-public void setCards(List<Card> cards) {
-	this.cards = cards;
-}
+	public void setCards(List<Card> cards) {
+		this.cards = cards;
+	}
 
-public List<Card> getCards() {
-	return cards;
-}
+	public List<Card> getCards() {
+		return cards;
+	}
 
 }
-	
-	
-
